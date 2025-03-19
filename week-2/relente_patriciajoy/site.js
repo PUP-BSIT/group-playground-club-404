376bb776324b568bd7b464e78dbe9dba6a70807d
relente-patriciajoy <relente.patriciajoy@gmail.com>
2025-03-19 17:10:54 +0800

    Disable comment button

--- week-2/relente_patriciajoy/site.js
+++ week-2/relente_patriciajoy/site.js
@@ -1,6 +1,6 @@
-const nameInput = document.getElementById("name");
-const commentInput = document.getElementById("comment");
-const commentBtn = document.getElementById("comment-btn");
+const nameInput = document.getElementById("comment_name");
+const commentInput = document.getElementById("comment_text");
+const commentBtn = document.getElementById("comment_btn");
 
 function checkInputs() {
     if (nameInput.value.trim() !== "" && commentInput.value.trim() !== "") {
