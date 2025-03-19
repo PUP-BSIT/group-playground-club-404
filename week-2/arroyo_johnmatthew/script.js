48e05329254b86080efc622751bbf7ec89170a86
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-03-19 17:31:51 +0800

    add javascript to the input field

---
+++ week-2/arroyo_johnmatthew/script.js
@@ -0,0 +1,10 @@
+function buttonEnabler() {
+	let name = document.getElementById("user_name");
+	let comment = document.getElementById("user_comment");
+
+	if(comment.value.length > 0 && name.value.length > 0) {
+		document.getElementById("comment_button").disabled = false;
+    } else {
+		document.getElementById("comment_btn").disabled = true;
+	}
+}
